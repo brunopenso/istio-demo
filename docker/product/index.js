b664227ab7c2481983a55a65ce3a8fe37d484009
Bruno Fuzetti Penso <bruno.penso@gmail.com>
2020-06-16 14:18:01 -0300

    service dependencie

--- docker/product/index.js
+++ docker/product/index.js
@@ -1,23 +1,44 @@
 var express = require('express');
 var app = express();
+var axios = require('axios');
+const port = process.env.APPLICATION_PORT ? process.env.APPLICATION_PORT : 3001
 
-app.get('/', function (req, res) {
-    const json = [
-        {  
-            "sku": 4433,
-            "name": "malbec",
-            "preco": 123.23
-        },
-        {  
-            "sku": 6643,
-            "name": "creme de maos nativa spa",
-            "preco": 30.23
-        },
-    ];
-
-  res.send(JSON.stringify(json));
+const products = [
+  {  
+      "sku": 4433,
+      "name": "mini cooper",
+      "preco": 12500
+  },
+  {  
+      "sku": 6643,
+      "name": "maxda rx7",
+      "preco": 10000.23
+  },
+  {
+    "sku": 1,
+    "name": "audi a3",
+    "preco": 20000
+  }
+];
+app.get('/', async (req, res) => {
+  var pTemp = products
+  for(i = 0; i< pTemp.length; i++) {
+    const response = await getData(pTemp[i].sku)
+    if (response.data) {
+      pTemp[i].description = response.data.desc
+    }
+  }
+  res.send(JSON.stringify(pTemp));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port', port);
 });
+
+const getData = async (code) => {
+  try {
+    return await axios.get('http://localhost:3000/'+code)
+  } catch (error) {
+    console.error('Axios error', error)
+  }
+}
